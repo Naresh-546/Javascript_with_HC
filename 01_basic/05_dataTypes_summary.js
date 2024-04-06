91d9074fed3c51dc958d2d6b797537e619618687
Naresh Choudhari <choudharinaresh0546@gmail.com>
2024-04-06 19:00:45 +0530

    dataTypes_summary

---
+++ 01_basic/05_dataTypes_summary.js
@@ -0,0 +1,54 @@
+/* Primitive data types :- 
+   1. String
+   2. Number
+   3. Boolean
+   4. null
+   5. undefined
+   6. symbol
+   7. BigInt
+*/ 
+
+/* Reference (Non-Primitive)
+    1. Array
+    2. Objects
+    3. Functions
+*/
+
+// Symbol data type :-
+
+const id   = Symbol("123");
+const id_1 = Symbol("123");
+
+console.log(id === id_1);
+console.log(typeof id);
+
+// BigInt data type :-
+
+const bigNumber = 54857498655498n
+console.log(bigNumber);
+console.log(typeof bigNumber);
+
+// Array :-
+
+const bio = ["Naresh", 23, "BCA", "Pune"]
+console.log(bio);
+console.log(typeof bio);
+
+// Objects : -  
+let myBio = {
+    name : "Naresh",
+    age : 23,
+    study : "BCA",
+    city : "Pune", 
+}
+console.log(myBio);
+console.log(typeof myBio);
+
+
+// Function
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction);
