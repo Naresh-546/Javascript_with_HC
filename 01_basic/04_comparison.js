2826acc9d0639e826e4bd679e2ee3581419efbe7
Naresh Choudhari <choudharinaresh0546@gmail.com>
2024-04-06 18:33:20 +0530

    comparison

---
+++ 01_basic/04_comparison.js
@@ -0,0 +1,37 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+
+
+/*
+
+Note :- comparison and equality operator works different.
+
+        1. Comparison operator convert null to a number it as 0
+        that's why ( 4th console null >= 0 ) is true
+
+        2. null > 0 is false.
+        
+
+*/
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+// strict check also data types
+
+console.log("2" == 2);
+console.log("2" === 2);
+console.log(2 === 2);
